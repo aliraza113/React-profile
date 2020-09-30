33d4a819e99189feac8f5b05f5d3807152de66c1
aliraza113 <jeeali113@gmail.com>
2020-09-30 02:26:20 -0700

    Updates

---
+++ precache-manifest.ab002b5647de408371115883f87d68a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c259333cad52499ae11b81ee2f397dd4",
+    "url": "/React-profile/index.html"
+  },
+  {
+    "revision": "4d16efe6c282fafe4121",
+    "url": "/React-profile/static/css/main.4076fd4e.chunk.css"
+  },
+  {
+    "revision": "13778c7545270a88adb8",
+    "url": "/React-profile/static/js/2.4a3f3188.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-profile/static/js/2.4a3f3188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d16efe6c282fafe4121",
+    "url": "/React-profile/static/js/main.105ff670.chunk.js"
+  },
+  {
+    "revision": "f389a45354f097a3f56d",
+    "url": "/React-profile/static/js/runtime-main.7c91da9f.js"
+  },
+  {
+    "revision": "6cec5e48eb32f7e7e8edcb19031af290",
+    "url": "/React-profile/static/media/android.6cec5e48.gif"
+  },
+  {
+    "revision": "87f087d64e154ed8c66d70b2934456e7",
+    "url": "/React-profile/static/media/apple.87f087d6.gif"
+  },
+  {
+    "revision": "404d52ee1c831a6eb49b5a2f3e3e5720",
+    "url": "/React-profile/static/media/nav.404d52ee.gif"
+  },
+  {
+    "revision": "26df737079de40c967f07df1148d9a1d",
+    "url": "/React-profile/static/media/react.26df7370.gif"
+  },
+  {
+    "revision": "6bf551444094d4cb16c7880b192c311b",
+    "url": "/React-profile/static/media/show.6bf55144.gif"
+  },
+  {
+    "revision": "077db905756a3d7e37e90d329252c846",
+    "url": "/React-profile/static/media/vu-2.077db905.gif"
+  }
+]);
